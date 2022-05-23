6f4739dd84cd4d5cefbe4b52b9cf2e3d7029c73f
Kevin Huang <kevinhuang960@gmail.com>
2022-05-22 21:55:14 -0400

    FinalProject_Selection

---
+++ FinalProject_Selection/FinalProject_Selection.pde
@@ -0,0 +1,91 @@
+/*
+
+*/
+import processing.serial.*;
+Serial myPort;
+
+int val=0;
+int circle = 200;
+float Red = random(256);
+float Green = random(256);
+float Blue = random(256);
+
+randomizer RandomValue;//Taking from class randomizer()
+float randomNum;
+
+void setup() {
+  size(1000, 1000); 
+  textSize (44);
+  textAlign (CENTER, CENTER);
+  RandomValue = new randomizer();//Initializing randomizer() to RandomValue
+
+  //set up Serial communication
+  printArray(Serial.list()); // prints port list to the console
+  String portName = Serial.list()[1]; //change to match your port
+  myPort = new Serial(this, portName, 9600); //initialize Serial communication at 9600 baud
+}
+
+void draw() {
+  background (255);
+  fill (Red, Green, Blue);
+  //Centerpiece button
+  ellipse(width/2, height/2, circle, circle);
+  fill(0);
+  text ("Click the buttons", width/2, height/2);
+  //Topside Buttons
+  fill(#0892d0);
+  rect(100, 100, 100, 100);
+  rect(800, 100, 100, 100);
+  rect(300, 100, 400, 100);
+  //Botside Buttons
+  rect(100, 800, 100, 100);
+  rect(800, 800, 100, 100);
+  rect(300, 800, 400, 100);
+  
+
+}
+
+void mousePressed(){
+  if(mouseX >= 100 && mouseX <= 200 && mouseY >= 100 && mouseY <= 200){
+    val = 10;
+  }
+  if(mouseX >= 300 && mouseX <= 700 && mouseY >= 100 && mouseY <= 200){
+    val = 20;
+  }
+  if(mouseX >= 800 && mouseX <= 900 && mouseY >= 100 && mouseY <= 200){
+    val = 30;
+  }
+  if(mouseX >= 100 && mouseX <= 200 && mouseY >= 800 && mouseY <= 900){
+    val = 40;
+  }
+  if(mouseX >= 300 && mouseX <= 700 && mouseY >= 800 && mouseY <= 900){
+    val = 50;
+  }
+  if(mouseX >= 800 && mouseX <= 900 && mouseY >= 800 && mouseY <= 900){
+    val = 60;
+  }
+  if(dist(mouseX, mouseY, width/2, height/2) < 100){//Circle mouse pressed
+    RandomValue.RandomNumber();//Random value and color fed everytime the button is pressed
+    RandomValue.RandomColor();
+  }
+    myPort.write(val); //write to Serial
+    println(val);  //print to console
+    
+}
+class randomizer{
+  //Randomizes a value between 70-200 to send to Arduino
+  public void RandomNumber(){
+    randomNum = random(70, 200);
+    val = int(randomNum);
+  }
+  //Randomizes the RGB
+  public void RandomColor(){
+    Red = random(256);
+    Green = random(256);
+    Blue = random(256);
+  }
+}
+/*
+Thanks to Odermonicon in the processing discourse for having a circle button.
+https://discourse.processing.org/t/how-do-you-turn-an-ellipse-into-a-button-and-when-clicked-it-would-show-the-data-on-the-same-screen/19366/7
+*/
---
+++ FinalProject_Selection/FinalProject_Selection/FinalProject_Selection.ino
@@ -0,0 +1,530 @@
+const int LED1 = 13;//LED instantiated
+const int LED2 = 12;
+const int LED3 = 11;
+const int LED4 = 10;
+const int LED5 = 9;
+const int LED6 = 8;
+const int BUZZER = 5; //buzzer to arduino pin 5
+int val = 0; //tracks incoming value from Serial port  
+int frequency=0; //tracks value to send to piezo
+volatile int State;
+
+void setup() {
+  pinMode(BUZZER, OUTPUT); 
+  pinMode(LED1, OUTPUT);
+  pinMode(LED2, OUTPUT);
+  pinMode(LED3, OUTPUT);
+  pinMode(LED4, OUTPUT);
+  pinMode(LED5, OUTPUT);
+  pinMode(LED6, OUTPUT);
+  Serial.begin (9600); //start serial communication at 9600 baud
+  pinMode(2, INPUT);
+}
+
+void loop() {
+  if (Serial.available()) { // If data is available to read
+    val = Serial.read(); // read it and store it in val
+    delay (10);
+  }
+    frequency = map (val, 0, 255, 100, 1500);
+    //StateReader - Changes the state depending on the value
+    if(val == 10){State = 0;}
+    else if(val == 20) {State = 1;}
+    else if(val == 30) {State = 2;}
+    else if(val == 40) {State = 3;}
+    else if(val == 50) {State = 4;}
+    else if(val == 60) {State = 5;}
+    else if(val <= 79) {State = 6;}
+    else if(val <= 119){State = 7;}
+    else if(val <= 149){State = 8;}
+    else if(val <= 179){State = 9;}
+    else if(val <= 200){State = 10;}
+    
+    //Piezo and LED changes depending on the states
+    if(State == 0){
+      digitalWrite(LED1, LOW);
+      digitalWrite(LED2, HIGH);
+      digitalWrite(LED3, HIGH);
+      digitalWrite(LED4, LOW);
+      digitalWrite(LED5, LOW);
+      digitalWrite(LED6, HIGH);
+      tone(BUZZER, 55.00, 150);
+      delay(160);
+      digitalWrite(LED1, HIGH);
+      digitalWrite(LED2, HIGH);
+      digitalWrite(LED3, HIGH);
+      digitalWrite(LED4, LOW);
+      digitalWrite(LED5, LOW);
+      digitalWrite(LED6, HIGH);
+      tone(BUZZER, 77.78, 200);
+      delay(210);
+      digitalWrite(LED1, HIGH);
+      digitalWrite(LED2, HIGH);
+      digitalWrite(LED3, HIGH);
+      digitalWrite(LED4, HIGH);
+      digitalWrite(LED5, LOW);
+      digitalWrite(LED6, HIGH);
+      tone(BUZZER, 207.65, 180);
+      delay(190);
+      digitalWrite(LED1, LOW);
+      digitalWrite(LED2, HIGH);
+      digitalWrite(LED3, LOW);
+      digitalWrite(LED4, HIGH);
+      digitalWrite(LED5, HIGH);
+      digitalWrite(LED6, HIGH);
+      tone(BUZZER, 440.00, 300);
+      delay(310);
+      digitalWrite(LED1, LOW);
+      digitalWrite(LED2, LOW);
+      digitalWrite(LED3, HIGH);
+      digitalWrite(LED4, HIGH);
+      digitalWrite(LED5, HIGH);
+      digitalWrite(LED6, LOW);
+      tone(BUZZER, 440.00, 300);
+      delay(310);
+    }
+    else if(State == 1){
+      digitalWrite(LED1, HIGH);
+      digitalWrite(LED2, HIGH);
+      digitalWrite(LED3, LOW);
+      digitalWrite(LED4, LOW);
+      digitalWrite(LED5, HIGH);
+      digitalWrite(LED6, HIGH);
+      tone(BUZZER, 55.00, 150);
+      delay(160);
+      digitalWrite(LED1, HIGH);
+      digitalWrite(LED2, LOW);
+      digitalWrite(LED3, LOW);
+      digitalWrite(LED4, LOW);
+      digitalWrite(LED5, LOW);
+      digitalWrite(LED6, HIGH);
+      tone(BUZZER, 293.665, 200);
+      delay(210);
+      digitalWrite(LED1, HIGH);
+      digitalWrite(LED2, LOW);
+      digitalWrite(LED3, HIGH);
+      digitalWrite(LED4, HIGH);
+      digitalWrite(LED5, LOW);
+      digitalWrite(LED6, HIGH);
+      tone(BUZZER, 55.00, 150);
+      delay(160);
+      digitalWrite(LED1, LOW);
+      digitalWrite(LED2, HIGH);
+      digitalWrite(LED3, LOW);
+      digitalWrite(LED4, LOW);
+      digitalWrite(LED5, HIGH);
+      digitalWrite(LED6, LOW);
+      tone(BUZZER, 440.00, 400);
+      delay(410);
+      digitalWrite(LED1, LOW);
+      digitalWrite(LED2, LOW);
+      digitalWrite(LED3, HIGH);
+      digitalWrite(LED4, HIGH);
+      digitalWrite(LED5, LOW);
+      digitalWrite(LED6, LOW);
+      tone(BUZZER, 123.47, 300);
+      delay(310);
+    }
+    if(State == 2){
+      digitalWrite(LED1, HIGH);
+      digitalWrite(LED2, HIGH);
+      digitalWrite(LED3, HIGH);
+      digitalWrite(LED4, LOW);
+      digitalWrite(LED5, HIGH);
+      digitalWrite(LED6, LOW);
+      tone(BUZZER, 293.665, 250);
+      delay(260);
+      digitalWrite(LED1, HIGH);
+      digitalWrite(LED2, LOW);
+      digitalWrite(LED3, HIGH);
+      digitalWrite(LED4, LOW);
+      digitalWrite(LED5, LOW);
+      digitalWrite(LED6, HIGH);
+      tone(BUZZER, 41.20, 100);
+      delay(110);
+      digitalWrite(LED1, HIGH);
+      digitalWrite(LED2, LOW);
+      digitalWrite(LED3, LOW);
+      digitalWrite(LED4, HIGH);
+      digitalWrite(LED5, LOW);
+      digitalWrite(LED6, HIGH);
+      tone(BUZZER, 293.665, 250);
+      delay(260);
+      digitalWrite(LED1, HIGH);
+      digitalWrite(LED2, LOW);
+      digitalWrite(LED3, HIGH);
+      digitalWrite(LED4, HIGH);
+      digitalWrite(LED5, LOW);
+      digitalWrite(LED6, LOW);
+      tone(BUZZER, 61.74, 250);
+      delay(260);
+      digitalWrite(LED1, LOW);
+      digitalWrite(LED2, HIGH);
+      digitalWrite(LED3, LOW);
+      digitalWrite(LED4, HIGH);
+      digitalWrite(LED5, HIGH);
+      digitalWrite(LED6, HIGH);
+      tone(BUZZER, 49.00, 275);
+      delay(285);
+      
+    }
+    if(State == 3){
+      digitalWrite(LED1, LOW);
+      digitalWrite(LED2, HIGH);
+      digitalWrite(LED3, LOW);
+      digitalWrite(LED4, HIGH);
+      digitalWrite(LED5, LOW);
+      digitalWrite(LED6, HIGH);
+      tone(BUZZER, 19.45, 300);
+      delay(310);
+      digitalWrite(LED1, HIGH);
+      digitalWrite(LED2, HIGH);
+      digitalWrite(LED3, LOW);
+      digitalWrite(LED4, LOW);
+      digitalWrite(LED5, LOW);
+      digitalWrite(LED6, HIGH);
+      tone(BUZZER, 61.74, 250);
+      delay(260);
+      digitalWrite(LED1, HIGH);
+      digitalWrite(LED2, LOW);
+      digitalWrite(LED3, HIGH);
+      digitalWrite(LED4, HIGH);
+      digitalWrite(LED5, LOW);
+      digitalWrite(LED6, LOW);
+      tone(BUZZER, 293.665, 250);
+      delay(260);
+      digitalWrite(LED1, LOW);
+      digitalWrite(LED2, LOW);
+      digitalWrite(LED3, HIGH);
+      digitalWrite(LED4, HIGH);
+      digitalWrite(LED5, LOW);
+      digitalWrite(LED6, HIGH);
+      tone(BUZZER, 41.20, 100);
+      delay(110);
+      digitalWrite(LED1, HIGH);
+      digitalWrite(LED2, LOW);
+      digitalWrite(LED3, HIGH);
+      digitalWrite(LED4, LOW);
+      digitalWrite(LED5, HIGH);
+      digitalWrite(LED6, LOW);
+      tone(BUZZER, 43.65, 100);
+      delay(110);
+      
+    }
+    if(State == 4){
+      digitalWrite(LED1, LOW);
+      digitalWrite(LED2, LOW);
+      digitalWrite(LED3, LOW);
+      digitalWrite(LED4, LOW);
+      digitalWrite(LED5, LOW);
+      digitalWrite(LED6, LOW);
+      tone(BUZZER, 41.20, 100);
+      delay(110);
+      digitalWrite(LED1, HIGH);
+      digitalWrite(LED2, LOW);
+      digitalWrite(LED3, HIGH);
+      digitalWrite(LED4, LOW);
+      digitalWrite(LED5, HIGH);
+      digitalWrite(LED6, LOW);
+      tone(BUZZER, 293.665, 250);
+      delay(260);
+      digitalWrite(LED1, LOW);
+      digitalWrite(LED2, HIGH);
+      digitalWrite(LED3, LOW);
+      digitalWrite(LED4, HIGH);
+      digitalWrite(LED5, HIGH);
+      digitalWrite(LED6, LOW);
+      tone(BUZZER, 61.74, 250);
+      delay(260);
+      digitalWrite(LED1, LOW);
+      digitalWrite(LED2, HIGH);
+      digitalWrite(LED3, LOW);
+      digitalWrite(LED4, HIGH);
+      digitalWrite(LED5, LOW);
+      digitalWrite(LED6, LOW);
+      tone(BUZZER, 293.665, 250);
+      delay(260);
+      digitalWrite(LED1, LOW);
+      digitalWrite(LED2, HIGH);
+      digitalWrite(LED3, LOW);
+      digitalWrite(LED4, LOW);
+      digitalWrite(LED5, LOW);
+      digitalWrite(LED6, LOW);
+      tone(BUZZER, 130.81, 250);
+      delay(260);
+      
+    }
+    if(State == 5){
+      digitalWrite(LED1, LOW);
+      digitalWrite(LED2, HIGH);
+      digitalWrite(LED3, LOW);
+      digitalWrite(LED4, HIGH);
+      digitalWrite(LED5, LOW);
+      digitalWrite(LED6, HIGH);
+      tone(BUZZER, 1108.73, 500);
+      delay(260);
+      digitalWrite(LED1, LOW);
+      digitalWrite(LED2, LOW);
+      digitalWrite(LED3, HIGH);
+      digitalWrite(LED4, LOW);
+      digitalWrite(LED5, HIGH);
+      digitalWrite(LED6, HIGH);
+      tone(BUZZER, 440.00, 250);
+      delay(110);
+      digitalWrite(LED1, HIGH);
+      digitalWrite(LED2, LOW);
+      digitalWrite(LED3, HIGH);
+      digitalWrite(LED4, LOW);
+      digitalWrite(LED5, LOW);
+      digitalWrite(LED6, LOW);
+      tone(BUZZER, 659.25, 300);
+      delay(260);
+      digitalWrite(LED1, LOW);
+      digitalWrite(LED2, HIGH);
+      digitalWrite(LED3, HIGH);
+      digitalWrite(LED4, HIGH);
+      digitalWrite(LED5, LOW);
+      digitalWrite(LED6, HIGH);
+      tone(BUZZER, 830.61, 275);
+      delay(260);
+      digitalWrite(LED1, HIGH);
+      digitalWrite(LED2, LOW);
+      digitalWrite(LED3, HIGH);
+      digitalWrite(LED4, LOW);
+      digitalWrite(LED5, HIGH);
+      digitalWrite(LED6, LOW);
+      tone(BUZZER, 1046.50, 325);
+      delay(285);
+      
+    }
+    if(State == 6){
+      digitalWrite(LED1, LOW);
+      digitalWrite(LED2, HIGH);
+      digitalWrite(LED3, LOW);
+      digitalWrite(LED4, HIGH);
+      digitalWrite(LED5, LOW);
+      digitalWrite(LED6, HIGH);
+      tone(BUZZER, 293.665, 250);//d
+      delay(260);
+      digitalWrite(LED1, HIGH);
+      digitalWrite(LED2, LOW);
+      digitalWrite(LED3, HIGH);
+      digitalWrite(LED4, LOW);
+      digitalWrite(LED5, HIGH);
+      digitalWrite(LED6, LOW);
+      tone(BUZZER, 293.665, 250);
+      delay(260);
+      digitalWrite(LED1, LOW);
+      digitalWrite(LED2, LOW);
+      digitalWrite(LED3, HIGH);
+      digitalWrite(LED4, HIGH);
+      digitalWrite(LED5, LOW);
+      digitalWrite(LED6, LOW);
+      tone(BUZZER, 2349.32, 500);//d7
+      delay(510);
+      digitalWrite(LED1, LOW);
+      digitalWrite(LED2, LOW);
+      digitalWrite(LED3, HIGH);
+      digitalWrite(LED4, HIGH);
+      digitalWrite(LED5, LOW);
+      digitalWrite(LED6, LOW);
+      tone(BUZZER, 55.00 , 500);//a
+      delay(510);
+      digitalWrite(LED1, LOW);
+      digitalWrite(LED2, HIGH);
+      digitalWrite(LED3, LOW);
+      digitalWrite(LED4, LOW);
+      digitalWrite(LED5, HIGH);
+      digitalWrite(LED6, LOW);
+      tone(BUZZER, 415.305 , 500);//g#
+      delay(510);
+      
+    }
+    if(State == 7){
+      digitalWrite(LED1, LOW);
+      digitalWrite(LED2, LOW);
+      digitalWrite(LED3, HIGH);
+      digitalWrite(LED4, HIGH);
+      digitalWrite(LED5, LOW);
+      digitalWrite(LED6, LOW);
+      tone(BUZZER, 415.305 , 500);//g
+      delay(510);
+      digitalWrite(LED1, HIGH);
+      digitalWrite(LED2, LOW);
+      digitalWrite(LED3, HIGH);
+      digitalWrite(LED4, HIGH);
+      digitalWrite(LED5, HIGH);
+      digitalWrite(LED6, LOW);
+      tone(BUZZER, 43.65 , 500);//f
+      delay(510);
+      digitalWrite(LED1, HIGH);
+      digitalWrite(LED2, HIGH);
+      digitalWrite(LED3, LOW);
+      digitalWrite(LED4, HIGH);
+      digitalWrite(LED5, HIGH);
+      digitalWrite(LED6, LOW);
+      tone(BUZZER, 293.665, 250);
+      delay(260);
+      digitalWrite(LED1, HIGH);
+      digitalWrite(LED2, LOW);
+      digitalWrite(LED3, HIGH);
+      digitalWrite(LED4, LOW);
+      digitalWrite(LED5, HIGH);
+      digitalWrite(LED6, HIGH);
+      tone(BUZZER, 43.65 , 250);
+      delay(260);
+      digitalWrite(LED1, HIGH);
+      digitalWrite(LED2, HIGH);
+      digitalWrite(LED3, LOW);
+      digitalWrite(LED4, HIGH);
+      digitalWrite(LED5, LOW);
+      digitalWrite(LED6, LOW);
+      tone(BUZZER, 415.305 , 250);
+      delay(260);
+      
+    }
+    if(State == 8){
+      digitalWrite(LED1, LOW);
+      digitalWrite(LED2, LOW);
+      digitalWrite(LED3, HIGH);
+      digitalWrite(LED4, HIGH);
+      digitalWrite(LED5, HIGH);
+      digitalWrite(LED6, LOW);
+      tone(BUZZER, 261.63 , 250);
+      delay(260);
+      digitalWrite(LED1, LOW);
+      digitalWrite(LED2, HIGH);
+      digitalWrite(LED3, HIGH);
+      digitalWrite(LED4, LOW);
+      digitalWrite(LED5, HIGH);
+      digitalWrite(LED6, LOW);
+      tone(BUZZER, 261.63 , 250);
+      delay(260);
+      digitalWrite(LED1, LOW);
+      digitalWrite(LED2, HIGH);
+      digitalWrite(LED3, HIGH);
+      digitalWrite(LED4, HIGH);
+      digitalWrite(LED5, LOW);
+      digitalWrite(LED6, LOW);
+      tone(BUZZER, 2349.32, 500);
+      delay(260);
+      digitalWrite(LED1, LOW);
+      digitalWrite(LED2, HIGH);
+      digitalWrite(LED3, LOW);
+      digitalWrite(LED4, HIGH);
+      digitalWrite(LED5, LOW);
+      digitalWrite(LED6, HIGH);
+      tone(BUZZER, 55.00 , 500);
+      delay(510);
+      digitalWrite(LED1, LOW);
+      digitalWrite(LED2, LOW);
+      digitalWrite(LED3, HIGH);
+      digitalWrite(LED4, HIGH);
+      digitalWrite(LED5, LOW);
+      digitalWrite(LED6, HIGH);
+      tone(BUZZER, 415.305 , 500);//g#
+      delay(510);
+      
+    }
+    if(State == 9){
+      digitalWrite(LED1, LOW);
+      digitalWrite(LED2, HIGH);
+      digitalWrite(LED3, HIGH);
+      digitalWrite(LED4, HIGH);
+      digitalWrite(LED5, HIGH);
+      digitalWrite(LED6, LOW);
+      tone(BUZZER, 415.305 , 500);//g
+      delay(510);
+      digitalWrite(LED1, HIGH);
+      digitalWrite(LED2, HIGH);
+      digitalWrite(LED3, LOW);
+      digitalWrite(LED4, LOW);
+      digitalWrite(LED5, HIGH);
+      digitalWrite(LED6, HIGH);
+      tone(BUZZER, 43.65 , 500);//f
+      delay(260);
+      digitalWrite(LED1, HIGH);
+      digitalWrite(LED2, HIGH);
+      digitalWrite(LED3, HIGH);
+      digitalWrite(LED4, HIGH);
+      digitalWrite(LED5, HIGH);
+      digitalWrite(LED6, HIGH);
+      tone(BUZZER, 293.665, 250);
+      delay(260);
+      digitalWrite(LED1, LOW);
+      digitalWrite(LED2, LOW);
+      digitalWrite(LED3, HIGH);
+      digitalWrite(LED4, HIGH);
+      digitalWrite(LED5, LOW);
+      digitalWrite(LED6, HIGH);
+      tone(BUZZER, 43.65 , 250);
+      delay(260);
+      digitalWrite(LED1, HIGH);
+      digitalWrite(LED2, LOW);
+      digitalWrite(LED3, LOW);
+      digitalWrite(LED4, HIGH);
+      digitalWrite(LED5, LOW);
+      digitalWrite(LED6, HIGH);
+      tone(BUZZER, 415.305 , 250);
+      delay(260);
+      
+    }
+    if(State == 10){
+      digitalWrite(LED1, LOW);
+      digitalWrite(LED2, HIGH);
+      digitalWrite(LED3, LOW);
+      digitalWrite(LED4, HIGH);
+      digitalWrite(LED5, LOW);
+      digitalWrite(LED6, HIGH);
+      tone(BUZZER, 61.74 , 250);
+      delay(260);
+      digitalWrite(LED1, HIGH);
+      digitalWrite(LED2, LOW);
+      digitalWrite(LED3, HIGH);
+      digitalWrite(LED4, LOW);
+      digitalWrite(LED5, HIGH);
+      digitalWrite(LED6, LOW);
+      tone(BUZZER, 61.74 , 250);
+      delay(260);
+      digitalWrite(LED1, LOW);
+      digitalWrite(LED2, LOW);
+      digitalWrite(LED3, HIGH);
+      digitalWrite(LED4, HIGH);
+      digitalWrite(LED5, LOW);
+      digitalWrite(LED6, LOW);
+      tone(BUZZER, 2349.32, 500);
+      delay(260);
+      digitalWrite(LED1, HIGH);
+      digitalWrite(LED2, LOW);
+      digitalWrite(LED3, HIGH);
+      digitalWrite(LED4, HIGH);
+      digitalWrite(LED5, LOW);
+      digitalWrite(LED6, HIGH);
+      tone(BUZZER, 55.00 , 500);
+      delay(260);
+      digitalWrite(LED1, LOW);
+      digitalWrite(LED2, LOW);
+      digitalWrite(LED3, HIGH);
+      digitalWrite(LED4, HIGH);
+      digitalWrite(LED5, LOW);
+      digitalWrite(LED6, LOW);
+      tone(BUZZER, 415.305, 500);
+      delay(510);
+      
+    }
+}
+
+void mousePressed()
+{//Whenever the mouse is pressed, it changes the state
+  if(val == 10){State == 0;}
+  else if(val == 20){State == 1;}
+  else if(val == 30){State == 2;}
+  else if(val == 40){State == 3;}
+  else if(val == 50){State == 4;}
+  else if(val == 60){State == 5;}
+  else if(val <= 79){State == 6;}
+  else if(val <= 119){State = 7;}
+  else if(val <= 149){State = 8;}
+  else if(val <= 179){State = 9;}
+  else if(val <= 200){State = 10;}
+}
